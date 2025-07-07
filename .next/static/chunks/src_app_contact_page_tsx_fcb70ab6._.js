6530c5fde3b74aea1386a6a024cbac2c18da2b5f
CHINTALA KARTHVYA <karthavyarvc@gmail.com>
2025-07-07 12:06:31 +0530

    Add .next/static/chunks/src_app_contact_page_tsx_fcb70ab6._.js 1751870195874

---
+++ .next/static/chunks/src_app_contact_page_tsx_fcb70ab6._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_b83d3412._.js",
+  "static/chunks/src_a4953b8f._.js"
+],
+    source: "dynamic"
+});
